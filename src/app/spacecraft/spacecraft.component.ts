54d95f9aa8d5ab8c6ee9389a34f525af5cdd1031
WilmerMoyano <wilmer95060425486@gmail.com>
2023-02-08 12:14:27 -0500

    Add spacecraft component

--- src/app/spacecraft/spacecraft.component.ts
+++ src/app/spacecraft/spacecraft.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {SpaceCraftService} from './spacecraft.service';
+import { ISpacecraft } from "./spacecraft";
 @Component({
   selector: 'app-spacecraft',
   templateUrl: './spacecraft.component.html',
   styleUrls: ['./spacecraft.component.scss']
 })
-export class SpacecraftComponent {
+export class SpacecraftComponent implements OnInit{
+  nave: any;
+  constructor(private spacecraftService: SpaceCraftService) {
+  }
 
+ngOnInit(): void {
+  let na: any;
+  na = {name:"saturnoV", country: "USA", releaseYear: 1990, state: true, fuelType:"nitro"};
+  this.nave = this.spacecraftService.createSpacecraft(na);
+  this.nave.fly();
+}
 }
